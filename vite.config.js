f45f7c47b8d84d18a2bb8c3cb877913ba911a491
Parasuraman-Annadurai <parasuramana@dckap.com>
2024-11-23 19:29:52 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
     return {
-        base: '/Zustand-project/',
-        plugins:[react()],
+        base: '/new-one/',
+        plugins: [react(), ghPages()],
         define: {
             'process.env.API_END_POINT': JSON.stringify(env.API_END_POINT),
         },
